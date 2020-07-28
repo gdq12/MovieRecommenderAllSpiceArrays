353a5b8b68d64ff2f82bfc0d2ba845d04a0a8eb6
gdq12 <gdquiceno@gmail.com>
2020-07-28 12:21:04 +0200

    massive backend modifications for cosim model incorporation along with ipynb for source code on object building

--- flask_app/recommender.py
+++ flask_app/recommender.py
@@ -1,60 +1,50 @@
 import pandas as pd
 from sklearn.decomposition import NMF
 import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 import pickle
-import json
 
 def sql_data(input_csv):
     '''
-    Takes the original data from postgres applies the specified filter and
-    produces the necessary input data and required varibles for other functions.
+    Imports premade input historical user data applicable to all models.
     '''
     Rtrue=pd.read_csv(input_csv, index_col=0)
-    num_movies=Rtrue.shape[1]
-    movieId=Rtrue.columns
-    return num_movies, movieId
+    return Rtrue
 
-def user_input_vector(user_flask, num_movies, movieId, movie_title_json):
+def user_input_vector(user_flask, Rtrue):
     '''
     Take the the user input dictionary from flask and converts it into a
     proper array for recommender function.
     '''
-    with open(movie_title_json, 'r') as fp:
-        movie_title_index = json.load(fp)
-    #create default vector
-    new_user_vector = pd.DataFrame([2.5]*num_movies, index=movieId).reset_index()
-    new_user_vector.columns=['movieId', 0]
-    new_user_vector=new_user_vector.replace({'movieId':movie_title_index}).set_index('movieId').T
-    #insert given user ratings to key movies
+    new_user_Id=len(Rtrue)+1
+    new_user_vector = pd.DataFrame([2.5]*Rtrue.shape[1], index=Rtrue.columns).T
     for key, value in user_flask.items():
         new_user_vector.loc[:, key] = float(value)
-    return new_user_vector, movie_title_index
+    new_user_vector.rename(index={0:new_user_Id},inplace=True)
+    return new_user_vector
 
-def recommender_build(pickle_file, new_user_vector, user_flask, movie_time_index, movie_title_index, movie_genre_index, movie_tag_index, movieId):
+def recommender_build(model, Rtrue, pickle_file, new_user_vector, user_flask, movie_time_index, movie_genres_index, movie_tags_index):
     '''
     Builds a recommendation table based on model and user input array previously built.
     '''
-    model = pickle.load(open(pickle_file, 'rb'))
-    with open(movie_time_index, 'r') as fp:
-        movie_time_index = json.load(fp)
-    with open(movie_tag_index, 'r') as fp:
-        movie_tag_index = json.load(fp)
-    with open(movie_genre_index, 'r') as fp:
-        movie_genre_index = json.load(fp)
-    #calculate recommendations based on model components
-    user_profile=model.transform(new_user_vector)
-    results=np.dot(user_profile, model.components_)
-    #customizing recommendation table with dictionary indices
-    results2=pd.DataFrame(results[0]).set_index(movieId).reset_index()
-    results2.columns=['movieId', 0]
-    results2['genre']=results2['movieId'].map(movie_genre_index)
-    results2['tag']=results2['movieId'].map(movie_tag_index)
+    if model == 'cosim_model':
+        new_user_Id=len(Rtrue)+1
+        R_new_user=Rtrue.append(new_user_vector)
+        simi_user_matrix=pd.DataFrame(cosine_similarity(R_new_user), index=R_new_user.index, columns=R_new_user.index)
+        simi_user_vector=simi_user_matrix[new_user_Id][~(simi_user_matrix.index==new_user_Id)]
+        results2=pd.DataFrame(np.dot(simi_user_vector, Rtrue)/simi_user_vector.sum(), index=Rtrue.columns).reset_index()
+    if model == 'bottom75_user_neutral' or model == 'top25_movies' or model == 'combo':
+        model1 = pickle.load(open(pickle_file, 'rb'))
+        user_profile=model1.transform(new_user_vector)
+        results=np.dot(user_profile, model1.components_)
+        results2=pd.DataFrame(results[0]).set_index(Rtrue.columns).reset_index()
+    results2.columns=['movieId', 'weight']
+    results2['genre']=results2['movieId'].map(movie_genres_index)
+    results2['tag']=results2['movieId'].map(movie_tags_index)
     results2['time_block']=results2['movieId'].map(movie_time_index)
-    results2.columns=['movieId', 'weight', 'genre', 'tag', 'time_block']
-    recommendations=results2.replace({'movieId':movie_title_index})
-    #remove already input movies
+    recommendations=results2
     for i in user_flask.keys():
-            recommendations.drop(recommendations.index[recommendations['movieId'] == i], inplace = True)
+        recommendations.drop(recommendations.index[recommendations.movieId== i], inplace = True)
     return recommendations
 
 def custom_recommendation(recommendations, search_filter, search_tag, num=5):
@@ -68,7 +58,3 @@ def custom_recommendation(recommendations, search_filter, search_tag, num=5):
     if search_filter == 'genre':
         recs=np.array(recommendations[recommendations['genre']==search_tag].sort_values('weight', ascending=True)['movieId'][:num])
     return recs
-
-# if __name__ == '__main__':
-#     for i in range(5):
-#         print(i)
